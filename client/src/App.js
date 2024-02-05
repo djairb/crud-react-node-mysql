ef135abb03507df460fe99ab3c5d2babf665170c
djairb <djair.batista@outlook.com>
2024-02-05 10:52:06 -0300

    fim da part2 - useEffect passando o map no array do mysql

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Axios from "axios";
+import Card from './componentes/cards/card';
+const apiUrl = "http://localhost:3001"
+
 
 
 function App() {
   const [values, setValues] = useState();
   //console.log(values);
+  const [listGames, setListGames] = useState();
+
+  console.log(listGames);
 
   const handleChangeValues = value =>(
 
@@ -21,7 +27,7 @@ function App() {
   /// ctrl + ; comenta e descomenta
 
   const handleClickButton = () => {
-    Axios.post("http://localhost:3001/register", {
+    Axios.post(apiUrl+"/register", {
       nome: values.nome,
       preco: values.preco,
       categoria: values.categoria,
@@ -30,49 +36,73 @@ function App() {
     })
   };
 
-  return <div className='app-container'>
-    <div className='register--container'>
+  useEffect(()=>{
+    ///frescuuuuraaaaaa
+    Axios.get("http://localhost:3001/getCards").then((response) =>{
+      setListGames(response.data)
+    });
 
-      <h1>Scrim Shop</h1>
+  }, []);
 
-      <input
-        type='text'
-        name='nome'
-        placeholder='Nome'
-        className='register--input'
-        onChange={handleChangeValues}
-      />
+  return (
+    <div className='app-container'>
+      <div className='register--container'>
 
-      <input
-        type='text'
-        name='preco'
-        placeholder='Preço'
-        className='register--input'
-        onChange={handleChangeValues}
-      />
+        <h1>Scrim Shop</h1>
 
-      <input
-        type='text'
-        name='categoria'
-        placeholder='Categoria'
-        className='register--input'
-        onChange={handleChangeValues}
-      />
+        <input
+          type='text'
+          name='nome'
+          placeholder='Nome'
+          className='register--input'
+          onChange={handleChangeValues}
+        />
 
-      <button
+        <input
+          type='text'
+          name='preco'
+          placeholder='Preço'
+          className='register--input'
+          onChange={handleChangeValues}
+        />
 
-      onClick={() => handleClickButton()}
+        <input
+          type='text'
+          name='categoria'
+          placeholder='Categoria'
+          className='register--input'
+          onChange={handleChangeValues}
+        />
+
+        <button
+
+        onClick={() => handleClickButton()}
+        
+        
+        >
+          Cadastrar
+        </button>
+          
+          
+          
+
+      </div>
+      {typeof listGames !== "undefined" &&
+
+      listGames.map((value) => {
+
+        return <Card></Card>
+
+        //ta retornando 4 por causa do arraymap que faz tipo um for
+
+
+
+      })};
       
-      
-      >
-        Cadastrar
-      </button>
-        
-        
-        
+
 
     </div>
-  </div>
+  );
 }
 
 export default App;
